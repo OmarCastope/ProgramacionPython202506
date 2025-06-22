ed88179d3e3927d40813f87bf51651aa14c81b6a
OmarCastope <rodeiza12@gmail.com>
2025-06-22 00:07:43 +0000

    clase1

---
+++ Modulo1/scripts/ejemplo2.py
@@ -0,0 +1,21 @@
+
+# Una panederia vende barras a 3.49 cada una. El pan que no es el dia tiene un descuento del 60%
+
+
+
+
+
+
+
+PRECIO_PAN = 3.49
+DESCUENTO = 0.6
+
+cantidad_barras = int(input('Ingrese el numero de pan vendidas que no son del dia: '))
+
+descuento_aplicado = DESCUENTO * cantidad_barras * PRECIO_PAN
+
+
+
+print(f'Precio batta')
+
+
---
+++ Modulo1/scripts/ejemplos.py
@@ -0,0 +1,23 @@
+
+# Ingrese la base y la altura de un triangulo, luego realize el calculo del área. Por último muestre el resultado en pantalla
+
+
+# 1.Ingreso de datos
+# funcion "input"
+base_triangulo = int(input())
+altura_triangulo = int(input())
+
+
+
+
+
+# 3. Imprimir resultado
+
+print(altura_triangulo)
+# Imprimiendo con "f" format string delante del texto
+print
+
+
+
+
+
